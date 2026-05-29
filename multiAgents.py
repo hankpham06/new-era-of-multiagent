87e5def052caabd165ca97303089578928ece315
Duy123343 <hpduy.tb@gmail.com>
2026-05-29 10:13:48 +0700

    Refactor code in multiAgents.py

--- multiAgents.py
+++ multiAgents.py
@@ -70,7 +70,7 @@ class ReflexAgent(Agent):
         # Useful information you can extract from a GameState (pacman.py)
         successorGameState = currentGameState.generatePacmanSuccessor(action)
         newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood()
+        newFood = successorGameState.getFood().asList()
         newGhostStates = successorGameState.getGhostStates()
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
